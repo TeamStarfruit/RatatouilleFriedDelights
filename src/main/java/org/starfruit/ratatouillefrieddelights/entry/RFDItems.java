50f5fa0eaa79617e2b44fe821ea32aac00326499
Neve133 <hzh15914420771@gmail.com>
2025-08-21 11:13:45 +0900

    fix: french_fries

--- src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDItems.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDItems.java
@@ -5,7 +5,7 @@ import org.starfruit.ratatouillefrieddelights.RatatouilleFriedDelights;
 import net.minecraft.world.item.Item;
 
 public class RFDItems {
-    public static final ItemEntry<Item> FRENCH_FRIED = RatatouilleFriedDelights.REGISTRATE.item("french_fried", Item::new).register();
+    public static final ItemEntry<Item> FRENCH_FRIES = RatatouilleFriedDelights.REGISTRATE.item("french_fries", Item::new).register();
 
     public static void register() {}
 }
