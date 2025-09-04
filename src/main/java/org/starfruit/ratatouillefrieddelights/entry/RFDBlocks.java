4919cc068e2d4a4ce9bcab4a10975e3fdbf68c67
Neve133 <hzh15914420771@gmail.com>
2025-09-04 20:51:49 +0900

    update: continuous fryer block

--- src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDBlocks.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDBlocks.java
@@ -1,25 +1,33 @@
 package org.starfruit.ratatouillefrieddelights.entry;
 
-import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
 import com.simibubi.create.foundation.data.AssetLookup;
-import com.simibubi.create.foundation.data.BlockStateGen;
-import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import org.starfruit.ratatouillefrieddelights.RatatouilleFriedDelights;
 
-import static com.simibubi.create.foundation.data.BlockStateGen.simpleCubeAll;
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 
 public class RFDBlocks {
 
+    static {
+        RatatouilleFriedDelights.REGISTRATE.setCreativeTab(RFDCreativeModeTabs.BASE_CREATIVE_TAB);
+    }
+
+    public static final BlockEntry<Block> CONTINUOUS_FRYER = RatatouilleFriedDelights.REGISTRATE
+            .block("continuous_fryer", Block::new)
+            .initialProperties(SharedProperties::stone)
+            .properties(p -> p.mapColor(MapColor.METAL).sound(SoundType.METAL))
+            .transform(pickaxeOnly())
+            .blockstate((ctx, prov) -> prov.simpleBlock(ctx.get(), AssetLookup.partialBaseModel(ctx, prov)))
+            .item()
+            .model((c, p) -> p.withExistingParent(c.getName(), RatatouilleFriedDelights.asResource("block/continuous_fryer/item")))
+            .transform(customItemModel())
+            .register();
+
     public static void register() {
     }
 }
-
-
