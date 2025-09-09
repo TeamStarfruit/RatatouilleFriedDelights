8bf4a41fdc03b0bc7552552040cccc57fac3d6d4
Neve133 <hzh15914420771@gmail.com>
2025-09-09 21:55:04 +0900

    add: recipes

--- src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/create/RFDMixingRecipeGen.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/create/RFDMixingRecipeGen.java
@@ -1,97 +1,84 @@
 package org.starfruit.ratatouillefrieddelights.data.recipe.create;
 
+import com.simibubi.create.AllItems;
 import com.simibubi.create.api.data.recipe.MixingRecipeGen;
+import com.simibubi.create.content.processing.recipe.HeatCondition;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.common.Tags;
 import org.forsteri.ratatouille.Ratatouille;
+import org.forsteri.ratatouille.entry.CRFluids;
+import org.forsteri.ratatouille.entry.CRItems;
+import org.starfruit.ratatouillefrieddelights.entry.RFDItems;
+import vectorwing.farmersdelight.common.registry.ModItems;
 
 import java.util.concurrent.CompletableFuture;
 
 @SuppressWarnings("unused")
 public class RFDMixingRecipeGen extends MixingRecipeGen {
-/*
+
     GeneratedRecipe
-            CAKE_BATTER = create("cake_batter", b -> b
-            .require(com.simibubi.create.AllItems.WHEAT_FLOUR.get())
-            .require(com.simibubi.create.AllItems.WHEAT_FLOUR.get())
-            .require(Items.SUGAR)
+            BURGER_BUN_MOLD_UNBAKED = create("burger_bun_mold_unbaked", b -> b
+            .require(AllItems.WHEAT_FLOUR.get())
+            .require(AllItems.WHEAT_FLOUR.get())
+            .require(RFDItems.BUTTER)
             .require(CRFluids.EGG_YOLK.get(), 250)
-            .require(Tags.Fluids.MILK, 250)
-            .output(CRFluids.CAKE_BATTER.get(), 1000)
+            .require(RFDItems.BURGER_BUN_MOLD)
+            .output(RFDItems.BURGER_BUN_MOLD_UNBAKED.get(), 1)
     ),
 
-    COCOA_PRESS = create("cocoa_press", b -> b
-            .require(CRFluids.COCOA_LIQUOR.get(), 250)
-            .output(CRItems.COCOA_SOLIDS.get())
-            .output(0.75f, CRItems.COCOA_BUTTER.get(), 1)
+    RAW_CHEESE = create("raw_cheese", b -> b
+            .require(Tags.Fluids.MILK, 1000)
+            .require(CRItems.SALT)
+            .output(RFDItems.RAW_CHEESE.get())
     ),
 
-    NIBS_TO_LIQUOR = create("nibs_to_liquor", b -> b
-            .require(CRItems.DRIED_COCOA_NIBS.get())
-            .output(CRFluids.COCOA_LIQUOR.get(), 250)
-            .requiresHeat(HeatCondition.HEATED)
+    RAW_FISH_FILLET = create("raw_fish_fillet", b -> b
+            .require(CRFluids.EGG_YOLK.get(), 250)
+            .require(CRItems.SALT)
+            .require(Tags.Items.FOODS_RAW_FISH)
+            .output(RFDItems.RAW_FISH_FILLET.get())
     ),
 
-    RESIDUE_SOLIDIFY = create("residue_solidify", b -> b
-            .require(CRFluids.COMPOST_RESIDUE_FLUID.get(), 200)
-            .output(CRItems.COMPOST_RESIDUE.get())
-            .requiresHeat(HeatCondition.HEATED)
+    RAW_CHICKEN_NUGGETS = create("raw_chicken_nuggets", b -> b
+            .require(AllItems.WHEAT_FLOUR.get())
+            .require(CRFluids.EGG_YOLK.get(), 250)
+            .require(CRItems.SALT)
+            .require(ModItems.CHICKEN_CUTS.get())
+            .output(RFDItems.RAW_CHICKEN_NUGGETS.get())
+            ),
+
+    SECRET_SEASONING_POWDER = create("secret_seasoning_powder", b -> b
+            .require(AllItems.WHEAT_FLOUR.get())
+            .require(Items.BEETROOT)
+            .require(Items.DRIED_KELP)
+            .require(CRItems.SALT)
+            .require(ModItems.ONION.get())
+            .output(RFDItems.SECRET_SEASONING_POWDER.get())
     ),
 
-    MINCE_MEAT = create("mince_meat", b -> b
-            .require(CRTags.RAW_MEAT)
-            .require(CRItems.SALT.get())
-            .output(CRFluids.MINCE_MEAT.get(), 250)
-    ),
-
-    CHOCOLATE = create("chocolate", b -> b
-            .require(CRItems.COCOA_SOLIDS.get())
-            .require(CRItems.COCOA_BUTTER.get())
+    PUFF_PASTRY = create("puff_pastry", b -> b
+            .require(AllItems.WHEAT_FLOUR.get())
             .require(Items.SUGAR)
-            .output(com.simibubi.create.AllFluids.CHOCOLATE.get(), 250)
+            .require(RFDItems.BUTTER)
+            .output(RFDItems.PUFF_PASTRY.get())
+    ),
+
+    CHICKEN_SO_BEAUTIFUL = create("chicken_so_beautiful", b -> b
+            .require(Items.CHICKEN)
+            .require(RFDItems.SECRET_SEASONING_POWDER)
+            .require(Items.EGG)
+            .output(RFDItems.RAW_ORIGINAL_RECIPE_CHICKEN_LEG.get())
+            .output(RFDItems.RAW_ORIGINAL_RECIPE_CHICKEN_BREAST.get())
+    ),
+
+    BUTTER = create("butter", b -> b
+            .require(Tags.Fluids.MILK, 500)
+            .output(RFDItems.BUTTER.get())
             .requiresHeat(HeatCondition.HEATED)
-    ),
+    );
 
-    ORGANIC_COMPOST = create("organic_compost", b -> b
-            .require(CRItems.COMPOST_RESIDUE.get())
-            .require(CRItems.COMPOST_RESIDUE.get())
-            .require(CRItems.COMPOST_RESIDUE.get())
-            .require(CRItems.COMPOST_RESIDUE.get())
-            .require(Items.DIRT)
-            .output(ModItems.ORGANIC_COMPOST.get())
-    ),
-
-    RESIDUE_TO_PULP = create("residue_to_pulp", b -> b
-            .require(CRItems.COMPOST_RESIDUE.get())
-            .require(CRItems.SALT.get())
-            .require(Fluids.WATER, 250)
-            .output(com.simibubi.create.AllItems.PULP.get())
-            .requiresHeat(HeatCondition.HEATED)
-    ),
-
-    SALT_FROM_BOIL = create("salt_from_boil", b -> b
-            .require(Fluids.WATER, 250)
-            .require(CRItems.BOIL_STONE.get())
-            .output(CRItems.SALT.get())
-            .output(CRItems.BOIL_STONE.get())
-            .requiresHeat(HeatCondition.HEATED)
-    ),
-
-    SALTY_DOUGH = create("salty_dough", b -> b
-            .require(com.simibubi.create.AllItems.WHEAT_FLOUR.get())
-            .require(CRItems.SALT.get())
-            .require(CRFluids.EGG_YOLK.get(), 100)
-            .output(CRItems.SALTY_DOUGH.get())
-    ),
-    MELON_JUICE = create("melon_juice", b -> b
-            .require(Items.MELON_SLICE)
-            .require(Items.MELON_SLICE)
-            .require(Items.MELON_SLICE)
-            .require(Items.MELON_SLICE)
-            .require(Items.SUGAR)
-            .output(CRFluids.MELON_JUICE_FLUID.get(), 500)
-            );
-*/
     public RFDMixingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, Ratatouille.MOD_ID);
     }
--- src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/ratatouille/ThreshingRecipeGen.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/ratatouille/ThreshingRecipeGen.java
@@ -9,47 +9,49 @@ import org.forsteri.ratatouille.Ratatouille;
 import org.forsteri.ratatouille.content.thresher.ThreshingRecipe;
 import org.forsteri.ratatouille.entry.CRItems;
 import org.forsteri.ratatouille.entry.CRRecipeTypes;
+import org.starfruit.ratatouillefrieddelights.entry.RFDItems;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
 import java.util.concurrent.CompletableFuture;
 
 @SuppressWarnings("unused")
 public final class ThreshingRecipeGen extends StandardProcessingRecipeGen<ThreshingRecipe> {
-/*
+
     GeneratedRecipe
-            RICE = this.create(
-            "rice",
+            COLA_NUTS_THRESHING = this.create(
+            "cola_nuts_threshing",
             b -> b
-                    .require(ModItems.RICE_PANICLE.get())
-                    .output(ModItems.RICE.get())
-                    .output(0.5F, ModItems.RICE.get()).duration(200)
-    ),
-
-    BOIL_STONE = this.create("boil_stone",
-            b -> b
-                    .require(Tags.Items.STONES)
-                    .output(CRItems.BOIL_STONE.get())
-                    .duration(200)
-    ),
-
-    DRIED_COCOA_BEANS = this.create(
-            "dried_cocoa_beans",
-            b -> b
-                    .require(CRItems.DRIED_COCOA_BEANS.get())
-                    .output(CRItems.DRIED_COCOA_NIBS.get(), 2)
-                    .output(0.5F, CRItems.DRIED_COCOA_NIBS.get())
-                    .duration(200)
-    ),
-
-    WHEAT_KERNELS = this.create(
-            "wheat_kernels",
-            b -> b
-                    .require(Items.WHEAT)
-                    .output(CRItems.WHEAT_KERNELS.get(), 2)
-                    .output(0.5F, CRItems.WHEAT_KERNELS.get())
-                    .duration(200)
+                    .require(RFDItems.COLA_FRUITS.get())
+                    .output(RFDItems.COLA_NUTS.get())
+                    .output(RFDItems.COLA_NUTS.get())
+                    .output(0.5F, RFDItems.COLA_NUTS.get(),2).duration(200)
     );
-*/
+    /*
+        BOIL_STONE = this.create("boil_stone",
+                b -> b
+                        .require(Tags.Items.STONES)
+                        .output(CRItems.BOIL_STONE.get())
+                        .duration(200)
+        ),
+
+        DRIED_COCOA_BEANS = this.create(
+                "dried_cocoa_beans",
+                b -> b
+                        .require(CRItems.DRIED_COCOA_BEANS.get())
+                        .output(CRItems.DRIED_COCOA_NIBS.get(), 2)
+                        .output(0.5F, CRItems.DRIED_COCOA_NIBS.get())
+                        .duration(200)
+        ),
+
+        WHEAT_KERNELS = this.create(
+                "wheat_kernels",
+                b -> b
+                        .require(Items.WHEAT)
+                        .output(CRItems.WHEAT_KERNELS.get(), 2)
+                        .output(0.5F, CRItems.WHEAT_KERNELS.get())
+                        .duration(200)
+        );
+    */
     public ThreshingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, Ratatouille.MOD_ID);
     }
