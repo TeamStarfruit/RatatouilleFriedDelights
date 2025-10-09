527e35f471fc48e4c00e5a1b9ad7b221bc6ee39d
Neve133 <hzh15914420771@gmail.com>
2025-10-09 09:56:04 +0900

    update: ratatouille version

--- src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/farmersdelights/FDCuttingRecipeGen.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/data/recipe/farmersdelights/FDCuttingRecipeGen.java
@@ -3,7 +3,6 @@ package org.starfruit.ratatouillefrieddelights.data.recipe.farmersdelights;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.starfruit.ratatouillefrieddelights.entry.RFDItems;
-import vectorwing.farmersdelight.common.tag.CommonTags;
 import vectorwing.farmersdelight.data.builder.CuttingBoardRecipeBuilder;
 import vectorwing.farmersdelight.data.recipe.CuttingRecipes;
 
@@ -14,8 +13,6 @@ public class FDCuttingRecipeGen extends CuttingRecipes {
     }
 
     private static void RFDCutting(RecipeOutput output) {
-        CuttingBoardRecipeBuilder.cuttingRecipe(Ingredient.of(RFDItems.BURGER_BUN), Ingredient.of(CommonTags.TOOLS_KNIFE), RFDItems.BOTTOM_BURGER_BUN.get(), 1)
-                .addResult(RFDItems.TOP_BURGER_BUN,1)
-                .build(output);
+
     }
 }
