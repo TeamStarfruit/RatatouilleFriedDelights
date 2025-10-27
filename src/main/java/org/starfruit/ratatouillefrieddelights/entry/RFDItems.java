ed4c582d801651ed852fb684da519d4fce1ac046
Neve133 <hzh15914420771@gmail.com>
2025-10-27 23:18:56 +0900

    add: ketchup dip cup

--- src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDItems.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDItems.java
@@ -40,6 +40,11 @@ public class RFDItems {
                     .build()))
             .register();
 
+    public static final ItemEntry<Item> A_FRIES = RatatouilleFriedDelights.REGISTRATE.item("a_fries", Item::new).properties(p -> p.food(new FoodProperties.Builder()
+                    .nutrition(2).saturationModifier(0.2f)
+                    .alwaysEdible()
+                    .build()))
+            .register();
     public static final ItemEntry<Item> BOXED_FRIES = RatatouilleFriedDelights.REGISTRATE.item("boxed_fries", Item::new).properties(p -> p.food(new FoodProperties.Builder()
                     .nutrition(6).saturationModifier(0.6f)
                     .alwaysEdible()
