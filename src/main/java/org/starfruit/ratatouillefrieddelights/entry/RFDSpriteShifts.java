25b9b31d46cd852f86fa61a7dbd6d01e61a6cf63
thaumstrial <2738722282@qq.com>
2025-10-29 01:04:31 +0800

    refactor: dip cup model

--- src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDSpriteShifts.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/entry/RFDSpriteShifts.java
@@ -6,6 +6,11 @@ import org.starfruit.ratatouillefrieddelights.RatatouilleFriedDelights;
 
 public class RFDSpriteShifts {
     public static final SpriteShiftEntry FRYER_BELT = get("block/continuous_fryer/ladder_andesite", "block/continuous_fryer/ladder_andesite_scroll");
+    public static final SpriteShiftEntry DIP_CUP_SOURCE = get("block/source");
+
+    private static SpriteShiftEntry get(String location) {
+        return get(location, location);
+    }
 
     private static SpriteShiftEntry get(String originalLocation, String targetLocation) {
         return SpriteShifter.get(RatatouilleFriedDelights.asResource(originalLocation), RatatouilleFriedDelights.asResource(targetLocation));
@@ -13,5 +18,6 @@ public class RFDSpriteShifts {
 
     public static void init() {
         var _belt = FRYER_BELT.toString();
+        var _dipCup = DIP_CUP_SOURCE.toString();
     }
 }
