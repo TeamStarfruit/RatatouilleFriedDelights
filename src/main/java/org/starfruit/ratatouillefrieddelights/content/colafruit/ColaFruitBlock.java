a7096ec7e87ab273483ed05cdfdac9799fd1c7bf
thaumstrial <2738722282@qq.com>
2026-04-28 04:51:48 +0200

    fix: #7

--- src/main/java/org/starfruit/ratatouillefrieddelights/content/colafruit/ColaFruitBlock.java
+++ src/main/java/org/starfruit/ratatouillefrieddelights/content/colafruit/ColaFruitBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
@@ -12,9 +13,16 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ColaFruitBlock extends Block {
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 2);
+    protected static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.box(6.0D, 7.0D, 6.0D, 10.0D, 16.0D, 10.0D),
+            Block.box(4.0D, 3.0D, 4.0D, 12.0D, 16.0D, 12.0D),
+            Block.box(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D)
+    };
 
     public ColaFruitBlock(BlockBehaviour.Properties props) {
         super(props);
@@ -24,6 +32,11 @@ public class ColaFruitBlock extends Block {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(AGE);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[state.getValue(AGE)];
     }
 
     // 只有在叶子下方才允许存活
